98c9dff4e06f9db81384f97c26eadeb54df3009b
ebalexander <ealexander@seas.harvard.edu>
2016-04-20 11:16:16 -0400

    implemented inserts with real data. added datamanager.py which reads from downloaded spreadsheet and writes to local machines' files. see testscript for brief operation notes. local modified to contain e0, and to write scores to log files, and to monitor the input file for changes, and to check against a threshold before sending reps. greedy now returns score with reps instead of printing it. to dos: listed in comments on each file, additionally: update central, write documentation, logging/running/experimental setup

---
+++ datamanager.py
@@ -0,0 +1,92 @@
+########### A data-generation simulator ###############
+# Reads data from a file or website,
+# writing it to file associated with machines,
+# selecting the machine uniformly at random.
+#
+# Input:
+#   source: csv file containing all test data
+#   initsize: number of datapoints written to files
+#             before long pause
+#   maxsize: total number of datapoints written to files
+#   timescale: pause between updates
+#   foracle: data file for oracle (used in scoring)
+#   fcentral: data file for central machine
+#   *args: data file for local machines
+#
+# Output:
+#    none, writes data to files as side effect
+#
+# Imports:
+#   itertools.islice: allows access to parts of files
+#   random.randint: generates random integers
+#   time.sleep: allows pause before and between inserts
+#
+# TODO:
+# - make sure update is slow enough for oracle?
+# - rather than long-pausing, start machines?
+# - read from website rather than downloaded source file??
+
+from itertools import islice
+from random import randint
+from time import sleep
+
+def datamanager(source,initsize,maxsize,timescale,foracle,fcentral,*args):
+    
+    # open source file
+    data = open(source,'r')
+    print 'source file open'
+    
+    # determine limits of data
+    if not maxsize:
+        maxsize = initsize;
+        # maxsize = sum(1 for datum in data) # size of source
+    initsize = min(initsize,maxsize)
+    
+    # open machine files
+    f = []
+    f.append(open(foracle,'w'))
+    f.append(open(fcentral,'w'))
+    for arg in args:
+        f.append(open(arg,'w'))
+    maxf = len(f)
+    print 'machine files open, ', maxf-2, 'local'
+
+    # write initial data to machine files
+    for datum in islice(data,1,initsize):
+        # randomly select machine for insert
+        finsert = randint(1,maxf-1)
+        f[finsert].write(datum)
+        # give all data to oracle
+        f[0].write(datum)
+
+    # close machine files and pause to allow other processes to start
+    for i in range(0,maxf-1):
+        f[i].close()
+    print 'data initialized'
+    sleep(50*timescale)
+    print 'beginning insertion'
+
+    # perform insertions
+    oracle = open(foracle,'a')
+    for datum in islice(data,initsize+1,maxsize):
+        # randomly select machine for insert
+        finsert = randint(1,maxf-1)
+        # write data to machine file
+        print 'inserting to machine ', finsert
+        if finsert == 1:
+            f = open(fcentral,'a')
+            f.write(datum)
+            f.close
+        else:
+            f = open(args[finsert-2],'a')
+            f.write(datum)
+            f.close
+        # give all data to oracle
+        oracle.write(datum)
+        # pause between insertions
+        sleep(timescale)
+
+    # after all data inserted, close all files
+    print 'out of data, closing files.', maxsize, ' datapoints used'
+    oracle.close()
+    data.close()
--- local.py
+++ local.py
@@ -2,33 +2,95 @@
 # A local machine first loads the data V it has on its machine, then computes
 # a subset of V that represent V well, and then sends that solution to the 
 # central server.
+#
 # Input:
 #   filename: a string that is the filename for the local data V
 #   k: an integer that is the cardinality constraint for the local solution
 #   portCentralMachine: an integer that is the port that the local solution
 #                       should be sent to
-#   e0: an auxiliary element
+#
+# Output:
+#   none, prints and sends to central the k best reps as side effects
+#
+# Imports:
+#   greedy: our module, see greedy.py
+#   socket: sends messages between local and central machines
+#   json: encodes objects to be sent over wire
+#   csv.reader: separates data elements without splitting on commas
+#               within quoted strings, as are found in our data
+#   os.stat: returns file information, inc time of last update st_mtime
+#
+# TODO:
+# - write dist fn between sets of reps
+# - cleverer sending scheme than whole new set?
 
 import greedy
 import socket
 import json
+from csv import reader
+from os import stat
 
-def main(filename, k, portCentralMachine, e0):
+def local(filename, k, portCentralMachine, logfile, threshold):
     
-    lines = open(filename, 'r')
+    # time of last file update
+    lastupdate = 0
+    
+    # open log
+    loclog = open(logfile,'w')
+    
+    # make dummy entry
+    linelen = 22 # magic number for now, num fields
+    e0 = "z,"*(linelen-1)+"z"
+    
+    # keep track of reps to central machine
+    reps = []
+    
+    # monitor source file for updates
+    while True:
 
-    V = []
-    for line in lines:
-        e = line.split(',')
-        e[-1] = e[-1][0:-1]
-        V.append(e)
+        if lastupdate < stat(filename).st_mtime:
+        
+            # update timestamp
+            lastupdate = stat(filename).st_mtime
+
+            # read data
+            lines = open(filename, 'r')
+            V = []
+            for line in reader(lines): #comma splitting, respects " "
+                if len(line) == linelen:
+                    V.append(line)
+            lines.close()
+
+            # find representatives
+            print '********************'
+            print '# data points ', len(V)
+            if len(V) <= k:
+                S = V
+                score = -1
+            else:
+                S,score = greedy.greedy(V,V,k,e0)
+            print '********************'
+            print S
+            
+            # record score (most important for oracle)
+            print 'time: ' + str(lastupdate) + '\t score: ' + str(score)
+            loclog.write(str(lastupdate) + '\t' + str(score) + '\n')
     
-    print V
-    S = greedy.greedy(V,V,k,e0)
-    print S
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('', portCentralMachine))
-    sock.send(json.dumps(S))
-    sock.close()
+            # compare S to preexisting reps
+            if reps:
+                diff = threshold*2 # TODO: dist(S,reps)
+            else:
+                reps = S
+                diff = threshold+1
     
-    
+            # send representatives to central machine
+            if diff > threshold:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(('', portCentralMachine))
+                sock.send(json.dumps(S))
+                sock.close()
+                reps = S
+
+
+
+
